fca2020d43d09b3b818e09f14e0db7388e21d715
EzequielGarcia1720 <eggarcia@fi.uba.ar>
2025-11-28 13:12:36 -0300

    Funcion newUser creada y funcionando con la base de datos

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,17 +2,22 @@ const express = require('express');
 //Funciones importadas
 const { 
     getAllUsers,
-    getUser 
+    getUser,
+    newUser
 } = require('./usuarios.js'); 
 
 const app = express();
 const port = 3030;
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
 
 //Rutass
+//USERS - USER
+
+//GET
 app.get("/api/v1/users", async (req, res) => {
     const users = await getAllUsers();
     res.json(users);
@@ -26,3 +31,21 @@ app.get("/api/v1/users/:id", async (req, res) => {
         res.status(404).json({ error: "Usuario no encontrado" });
     }
 });
+
+//POST
+app.post("/api/v1/users", async (req, res) => {
+    const { username, psswd, email, firstname, lastname, tel, ubication } = req.body;
+    try {
+        await newUser(username, psswd, email, firstname, lastname, tel, ubication);
+        res.status(201).json({ message: "Usuario creado exitosamente" });
+    } catch (error) {
+        console.error("Error al crear el usuario:", error);
+        res.status(500).json({ error: "Error al crear el usuario" });
+    }
+});
+
+//PUT
+
+
+
+//DELETE
--- backend/src/usuarios.js
+++ backend/src/usuarios.js
@@ -9,12 +9,13 @@ const dbClient = new Pool({
 });
 
 
-
+//GetAllUsers
 async function getAllUsers() {
     const response = await dbClient.query("SELECT * FROM users");
     return response.rows; // ✅ Retorna los resultados
 }
 
+//GetUser
 async function getUser(id) {
     const response = await dbClient.query(
         "SELECT * FROM users WHERE id = $1",
@@ -28,8 +29,26 @@ async function getUser(id) {
     return response.rows[0]; 
 };
 
+//NewUser
+async function newUser(username,psswd,email,firstname,lastname,tel,ubication) {
+    const reponse = await dbClient.query(
+        "INSERT INTO users (username,psswd,email,firstname,lastname,tel,ubication) VALUES ($1,$2,$3,$4,$5,$6,$7)",
+        [username,psswd,email,firstname,lastname,tel,ubication]
+    )
+    return {
+        username,
+        psswd,
+        email,
+        firstname,
+        lastname,
+        tel,
+        ubication
+    }
+};
 
 module.exports = {
     getAllUsers,
-    getUser
+    getUser,
+    newUser
+
 };
