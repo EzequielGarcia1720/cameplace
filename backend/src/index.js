3e29fb473319a649805453b1879422e3e5a0f5ea
Lean3232 <lcejas@fi.uba.ar>
2026-01-25 20:18:08 -0300

    Página de mis subastas, donde se cargan mediante la base de datos, y se pueden editar, pausar y eliminar

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 // Importar rutas
 const usuariosRoutes = require('./routes/usuarios.routes');
@@ -10,6 +11,7 @@ const condicionesRoutes = require('./routes/condiciones.routes');
 const app = express();
 const port = 3030;
 
+app.use(cors());
 app.use(express.json());
 
 // Configurar rutas
