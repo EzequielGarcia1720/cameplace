f5d563ebc1fcbd1c15f901e4c3cbd2f381316e3e
Alex Espindola <alexespindola.info@gmail.com>
2026-01-25 00:37:56 -0300

    agrego rutas de mejorar oferta y eliminar oferta(ofertas_routes.js) con sus respectivas funciones(ofertas.js)

--- backend/src/ofertas.js
+++ backend/src/ofertas.js
@@ -7,9 +7,8 @@ const dbClient = new Pool({
     port: 5432,
     database: "cameplace"
 });
-
+// Obtener todas las ofertas, con opción de filtro
 async function GetAllOffers(querySQL, parameters) {
-
     try {
         const response = dbClient.query(querySQL, parameters)
         return response.rows
@@ -18,8 +17,59 @@ async function GetAllOffers(querySQL, parameters) {
     }
 }
 
+// Obtener una oferta por ID
+async function GetOffert(id) {
+    const response = await dbClient.query(
+        "SELECT * FROM ofertas WHERE id = $1", 
+        [id]
+    )
+    if (response.rows.length === 0)
+        return undefined
+    return response.rows[0]
+}
 
+// Mejorar oferta (crear nueva oferta)
+async function CreateOffert(id, type_offert, title, description, images_urls, amount, auctioneer_id, bidder_id, auction_id, creation_date) {
+    try {
+        const result = await dbClient.query(
+            "INSERT INTO ofertas(id, type_offert, title, description, images_urls, amount, auctioneer_id, bidder_id, auction_id, creation_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+            [id, type_offert, title, description, images_urls, amount, auctioneer_id, bidder_id, auction_id, creation_date]
+        )
+        if  (result.rowCount === 0) {
+            return undefined
+        }
+    } catch {
+        return undefined
+    }
 
+    return {
+        id,
+        type_offert,
+        title,
+        description,
+        images_urls,
+        amount,
+        auctioneer_id,
+        bidder_id,
+        auction_id,
+        creation_date
+    }
+}
+
+// Eliminar una oferta por ID
+async function RemoveOffer(id) {
+    try {
+        const result = await dbClient.query("DELETE FROM ofertas where id = $1", [id])
+        return result.rowCount === 1;
+    } catch {
+        return false
+    }
+}
+
+// Exportamos las funciones para usarlas en otras partes de la aplicación
 module.exports = {
     GetAllOffers,
+    GetOffert,
+    CreateOffert,
+    RemoveOffer
 }
--- backend/src/routes/ofertas.routes.js
+++ backend/src/routes/ofertas.routes.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const {
-    GetAllOffers
+    GetAllOffers,
+    GetOffert,
+    CreateOffert,
+    RemoveOffer
 } = require("../ofertas")
 
 // GET. /api/v1/offers
@@ -29,4 +32,57 @@ app.get("/", async (req, res) => {
     }
 });
 
+//POST. /api/v1/offers
+router.post("/", async (req, res) => {
+    if (req.body === undefined)
+        return res.status(400).send("No body was provided");
+    
+    const id = req.body.id;
+    const type_offert = req.body.type_offert;
+    const title = req.body.title;
+    const description = req.body.description;
+    const images_urls = req.body.images_urls;
+    const amount = req.body.amount;
+    const auctioneer_id = req.body.auctioneer_id;
+    const bidder_id = req.body.bidder_id;
+    const auction_id = req.body.auction_id;
+    const creation_date = req.body.creation_date;
+    
+    if (id === undefined)
+        return res.status(400).send("Number not provided");
+    if (await GetAuction(id) !== undefined)
+        return res.status(409).send("The auction already exists");
+    if (type_offert === undefined) 
+        return res.status(400).send("Type of offer not provided");
+    if (title === undefined) 
+        return res.status(400).send("Title not provided");
+    if (description === undefined) 
+        return res.status(400).send("Description not provided");
+    if (amount === undefined) 
+        return res.status(400).send("Amount not provided");
+    if (auctioneer_id === undefined) 
+        return res.status(400).send("Auctioneer ID not provided");
+    if (bidder_id === undefined) 
+        return res.status(400).send("Bidder ID not provided");
+    if (auction_id === undefined) 
+        return res.status(400).send("Auction ID not provided");
+    if (creation_date === undefined) 
+        return res.status(400).send("Creation date not provided");
 
+    const offert = await CreateOffert(id, title, description, amount, auctioneer_id, bidder_id, auction_id, creation_date);
+    
+    if (offert === undefined)
+        res.sendStatus(500);
+    res.status(201).json(offert);
+});
+
+//DELETE. /api/v1/offers/:id
+router.delete("/:id", async (req, res) => {
+    const offert = await GetOffert(req.params.id);
+    if (offert === undefined)
+        return res.sendStatus(404);
+    if (!(await RemoveOffer(req.params.id))) {
+        return res.sendStatus(500);
+    }
+    return res.json(offert);
+});
