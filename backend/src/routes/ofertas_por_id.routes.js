7c1802d0548d41852a0e8041e12feb2e02ed4a03
Lean3232 <lcejas@fi.uba.ar>
2026-02-01 18:23:54 -0300

    Escribo comentarios del código en auction.html, ofertas_por_id.routes.js y seeoffers.html

--- backend/src/routes/ofertas_por_id.routes.js
+++ backend/src/routes/ofertas_por_id.routes.js
@@ -5,13 +5,14 @@ const {
     UpdateStateOfTheOffer
 } = require("../ofertas");
 
+// Obtengo los datos de la oferta cuando veo las ofertas de tal subasta
 router.get("/:id", async (req, res) => {
     const offers = await GetOffer(req.params.id);
     if (offers === undefined)
         return res.sendStatus(404)
     res.json(offers);
 });
-
+// Actualiza el estado de la oferta cuando se acepta
 router.patch("/:id", async (req, res) => {
     let offers = await GetOffer(req.params.id);
     if (offers === undefined)
