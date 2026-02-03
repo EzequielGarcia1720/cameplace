907f78058bb7b6206cc58c5b79e501c8570831c1
Pipetorres13 <pippetorress@gmail.com>
2026-02-03 19:43:39 -0300

    Agrego numero de telefono en el perfil

--- backend/src/routes/usuarios.routes.js
+++ backend/src/routes/usuarios.routes.js
@@ -72,9 +72,9 @@ router.post("/", async (req, res) => {
 // PUT /api/v1/users/:id
 router.put("/:id", async (req, res) => {
     const userID = req.params.id;
-    const { username, firstname, lastname, biography, image_url } = req.body;
+    const { username, firstname, lastname, biography, image_url, tel } = req.body;
     try {
-        const updatedUser = await updateUser(userID, username, firstname, lastname, biography, image_url);
+        const updatedUser = await updateUser(userID, username, firstname, lastname, biography, image_url, tel);
         if (!updatedUser) {
             return res.status(404).json({ error: "Usuario no encontrado" });
         }
